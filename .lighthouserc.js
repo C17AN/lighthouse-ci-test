51603dd9cfd5d320a028ae995b1ffa8fca2f8dee
김찬민 <chanmin@gimchanmin-ui-MacBookPro.local>
2023-03-18 01:09:58 +0900

    chore: add CI

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,12 +2,23 @@
 
 module.exports = {
   ci: {
+    assert: {
+      assetions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
+        "categories:pwa": ["warn", { minScore: 0.9 }],
+      },
+    },
     collect: {
-      staticDistDir: "./build",
+      staticDistDir: "./.next/server/pages",
       // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
 
       // 둘 이상의 URL을 배열로 받아 모든 URL을 테스트해볼 수 있다.
       url: ["http://localhost:3000"],
+
+      // 실행 횟수
       numberOfRuns: 5,
     },
     upload: {
